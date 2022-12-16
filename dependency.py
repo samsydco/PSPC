80d6bdf3f1a47ad096f15deda184502c579670b4
Samantha Cohen <samantha.s.cohen@gmail.com>
2022-12-15 20:11:38 -0500

    Finished converting dependency from matlab code to python code.

--- dependency.py
+++ dependency.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-def dependency(res2,pair,guess=1,c=4):
+def dependency(res,pair,guess=1,c=4):
 	#function to calculate behavioural dependency measure
 	# Modified version of Matlab function from Aidan J Horner 04/2015
 
@@ -20,39 +20,38 @@ def dependency(res2,pair,guess=1,c=4):
 
 	# output:
 	# dep       =   dependency measure for [data independent_model dependent_model]
-	
+	dep = np.zeros((3))
 	# calculate dependency for data
 	res2 = res[pair]
-	dep_1      = sum(res2.sum(axis=1)==2)/len(res2) # calculate dependency for data
+	dep[0]      = sum(res2.sum(axis=1)==2)/len(res2) # calculate dependency for data
 
 	# calculate dependency for independent model
 
 	acc        = res2.mean() # calculate accuracy for each retrieval type
-	dep_2      = acc[0]*acc[1] # calculate dependenct for independent model
+	dep[1]      = acc[0]*acc[1] # calculate dependenct for independent model
 
 	# calculate dependency for dependent model
 
-	cont        = nan(size(res2,1),2,2) # create matrix for dependent model probabilities
+	cont        = np.empty((len(res2),2,2))*np.nan # create matrix for dependent model probabilities
 	g           = (1-res.values.mean())*(c/(c-1)) # calculate level of guessing
-	b           = np.mean(res); b(:,pair) = nan # calculate average performance
+	b           = np.mean(res); b[pair] = np.nan # calculate average performance
 	for i, a in res.iterrows(): # loop through all event   
-		a(:,pair) = nan;                    # calculate event specific performance
-		E       = nanmean(a)/nanmean(b);    # calculate ratio of event / average performance (episodic factor)
-		for p = 1:2;
-			if E*acc(p)>1
-				P(p) = 1;
-			else
-				if guess == 1
-					P(p) = (E*(acc(p)-(g/c)))+(g/c);
-				elseif guess == 0
-					P(p) = E*acc(p);
-				end
-			end
-		end
-		cont(i,1,1) = P(1)*P(2);
-		cont(i,1,2) = (1-P(1))*P(2);
-		cont(i,2,1) = P(1)*(1-P(2));
-		cont(i,2,2) = (1-P(1))*(1-P(2));
-	end
-	cont2       = squeeze(sum(cont));                           % create contingency table
-	dep(3)      = (cont2(1,1)+cont2(2,2))/sum(cont2(:));        % calculate dependency for dependent model
+		a[pair] = np.nan  # calculate event specific performance
+		E       = a.mean()/b.mean() # calculate ratio of event / average performance (episodic factor)
+		P = np.zeros((2))*np.nan
+		for ii,p in enumerate(pair):
+			if E*acc[p]>1:
+				P[ii] = 1
+			else:
+				if guess == 1:
+					P[ii] = (E*(acc[p]-(g/c)))+(g/c)
+				elif guess == 0:
+					P[ii] = E*acc[p]
+		cont[i,0,0] = P[0]*P[1]
+		cont[i,0,1] = (1-P[0])*P[1]
+		cont[i,1,0] = P[0]*(1-P[1])
+		cont[i,1,1] = (1-P[0])*(1-P[1])
+
+	cont2       = sum(cont) # create contingency table
+	dep[2]      = (cont2[0,0]+cont2[1,1])/np.sum(cont2) # calculate dependency for dependent model
+	return dep
