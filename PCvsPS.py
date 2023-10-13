4870f982312c0a9a5f4815fd6f2e331b1bca043d
Samantha Cohen <samantha.s.cohen@gmail.com>
2023-10-13 13:08:32 -0400

    Modifications to PCvsPS.py.

--- PCvsPS.py
+++ PCvsPS.py
@@ -6,28 +6,39 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from partial_regression import partial_regression
+
+year = 1
 
 figurepath = 'C:/Users/tuq67942/OneDrive - Temple University/Documents/Figures/'
-PSdf = pd.read_csv('PS.csv')
-Dependencydf = pd.read_csv('Dependency.csv')
+PSdf = pd.read_csv('PS_Year_'+str(year)+'.csv')
+PScatdf = pd.read_csv('PS_cat_Year_'+str(year)+'.csv')
+Dependencydf = pd.read_csv('Dependency_Year_'+str(year)+'.csv')
 datadf = pd.read_csv('datadf.csv')
+datadf = datadf[datadf['Year'] == year]
 
 subj = datadf['Subject'].unique()
 subj2 = Dependencydf['Subject'].unique()
 subj3 = PSdf['Subject'].unique()
 # Subject 2 is missing MDEM078 due to perfect accuracy!
 
+PScols = {'object':'Object Target Selection Rate','location':'Location Target Selection Rate','animal':'Animal Target Selection Rate'}
+
 output = []
-for subj in subj2:
-	tmp_demo = datadf[datadf['Subject']==subj]
-	tmp_ps = PSdf[PSdf['Subject']==subj]
-	tmp_pc = Dependencydf[Dependencydf['Subject']==subj]
+for subject in subj2:
+	tmp_demo = datadf[datadf['Subject']==subject]
+	tmp_ps = PSdf[PSdf['Subject']==subject]
+	tmp_ps_cat = PScatdf[PScatdf['Subject']==subject]
+	tmp_pc = Dependencydf[Dependencydf['Subject']==subject]
 	output.append({
 		'Subject':subj,
 		'Age':tmp_demo['Age'].values[0],
 		'KBIT':tmp_demo['kbit_std_v'].values[0],
 		'Delay':tmp_demo['Delay'].values[0],
 		'Target Selection Rate':tmp_ps[tmp_ps['Selection']=='Target']['Proportion Selected'].iloc[0],
+		'Object Target Selection Rate':tmp_ps_cat['object target selection rate'].iloc[0],
+		'Location Target Selection Rate':tmp_ps_cat['location target selection rate'].iloc[0],
+		'Animal Target Selection Rate':tmp_ps_cat['animal target selection rate'].iloc[0],
 		'Lure Selection Rate':tmp_ps[tmp_ps['Selection']=='Lure']['Proportion Selected'].iloc[0],
 		'Foil Selection Rate':tmp_ps[tmp_ps['Selection']=='Foil']['Proportion Selected'].iloc[0],
 		'Dependency':tmp_pc['Dependency'].values[0],
@@ -49,23 +60,40 @@ f.savefig(figurepath+'heatmap.png', bbox_inches='tight', dpi=100)
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
 from statsmodels.stats.anova import anova_lm
-olsdf = outputdf.rename({'Target Selection Rate': 'PS_acc', 'Associative Recognition': 'PC_acc'}, axis='columns')
+olsdf = outputdf.rename({'Target Selection Rate': 'PS_acc', 'Associative Recognition': 'PC_acc','Object Target Selection Rate':'object','Location Target Selection Rate':'location','Animal Target Selection Rate':'animal'}, axis='columns')
 model_acc = ols("PC_acc ~ PS_acc + KBIT + Age + Delay", data=olsdf).fit()
 print(model_acc.summary())
-model_dep = ols("Dependency ~ PS_acc + KBIT + Age", data=olsdf).fit()
+model_dep = ols("Dependency ~ PS_acc + KBIT + Age + Delay", data=olsdf).fit()
 print(model_dep.summary())
+model_pc_dep = ols("Dependency ~ PC_acc + KBIT + Age + Delay", data=olsdf).fit()
+print(model_pc_dep.summary())
+d = {'PSPC':model_acc,'PS-Dep':model_dep,'PC-Dep':model_pc_dep}
+for k,model in d.items():
+	var='PS_acc' if 'PS' in k else 'PC_acc'
+	df = model.df_resid
+	tval = model.tvalues[var]
+	rval = np.sqrt(np.square(tval) / (np.square(tval) + df))
+	pval = model.pvalues[var]
+	print('For model: '+k+' r('+str(df)+')='+str(rval)+', p = '+str(pval))
+for y in ['PC_acc','Dependency']:
+	for cat in PScols.keys():
+		model = ols(y+" ~ "+cat+" + KBIT + Age + Delay", data=olsdf).fit()
+		print(model.summary())
+		df = model.df_resid
+		tval = model.tvalues[cat]
+		rval = np.sqrt(np.square(tval) / (np.square(tval) + df))
+		pval = model.pvalues[cat]
+		print('For model '+y+' vs. PS-'+cat+': r('+str(df)+')='+str(rval)+', p = '+str(pval))
 
 # plot partial regression
 from sklearn.linear_model import LinearRegression
-nonadf = olsdf.dropna()
-PS_regression = LinearRegression().fit(nonadf[["KBIT","Age","Delay"]], nonadf["PS_acc"])
-PC_regression = LinearRegression().fit(nonadf[["KBIT","Age","Delay"]], nonadf["PC_acc"])
-PS_residuals = nonadf["PS_acc"] - PS_regression.predict(nonadf[["KBIT","Age","Delay"]])
-PC_residuals = nonadf["PC_acc"] - PC_regression.predict(nonadf[["KBIT","Age","Delay"]])
-residualsdf = pd.DataFrame({'Pattern Completion Accuracy': PC_residuals, 'Pattern Seperation Accuracy': PS_residuals})
-f, ax = plt.subplots(figsize=(6, 6))
-g1 = sns.regplot(data=residualsdf,x="Pattern Seperation Accuracy", y="Pattern Completion Accuracy" ,ax=ax)
-f.savefig(figurepath+'PS_vs_PS_given_KBIT_Age.png', bbox_inches='tight', dpi=100)
+for y,y_ in {'PC_acc':'Pattern Completion Accuracy','Dependency':'Dependency'}.items():
+	for x,x_ in dict({"PC_acc":'Pattern Completion Accuracy',"PS_acc":'Pattern Seperation Accuracy'}, **PScols).items():
+		if x!=y:
+			figname = figurepath+x+'_vs_'+y+'_given_KBIT_Age'+'.png'
+			partial_regression(olsdf, x, y, x_, y_,["KBIT","Age","Delay"], False, figname)
+			
+
 	
 	
 
