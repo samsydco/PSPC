0f7c181f04f5cc637dcced8da5c55daf9e00470b
Samantha Cohen <samantha.s.cohen@gmail.com>
2023-10-16 14:57:51 -0400

    Migrating code from PS.py to Results.ipynb.

--- PS.py
+++ PS.py
@@ -35,65 +35,29 @@ for subject,tmp in tqdm.tqdm(Contdict[year].items()):
 	d = d_.copy()
 	for col in PScols:
 		d[col+' target selection rate'] = np.count_nonzero(tmp[col] == 1) / (nitems/3)
-	percatoutput.append(d)
 	for value in PSkey.keys():
 		d2 = d_.copy()
 		d2['Selection'] = PSkey[value]
 		d2['Proportion Selected']  = np.count_nonzero(PStmp == value) / nitems
+		d[PSkey[value]] = d2['Proportion Selected']
 		output.append(d2)
+	percatoutput.append(d)
 	
 outputdf = pd.DataFrame(output)
+outputdf.to_csv('csvs/PSoutputdf_exactage.csv',index=False)
 percatoutputdf = pd.DataFrame(percatoutput)
-outputdf.to_csv('PS_Year_'+str(year)+'.csv',index=False)
-percatoutputdf.to_csv('PS_cat_Year_'+str(year)+'.csv',index=False)
+outputdf.to_csv('csvs/PS_Year_'+str(year)+'.csv',index=False)
+percatoutputdf.to_csv('csvs/PS_cat_Year_'+str(year)+'.csv',index=False)
 outputdf['Age'] = outputdf['Age'].map(lambda age: math.floor(age))
 delaydf = outputdf[(outputdf.Delay)]
 nodelaydf = outputdf[(outputdf.Delay == False)]
 
+outputdf.to_csv('csvs/PSoutputdf.csv',index=False)
+delaydf.to_csv('csvs/PSdelaydf.csv',index=False)
+
 order = ['Target','Lure','Foil']
 orderage = [4,5,6,7]
 
-
-sns.set(font_scale=2)
-fig, ax = plt.subplots(figsize=(7, 6))
-sns.boxplot(data=outputdf, x="Selection", y="Proportion Selected", hue="Age", palette="vlag",order=order, showfliers = False)
-sns.stripplot(data=outputdf, x="Selection", y="Proportion Selected", hue="Age", dodge=True,color=".3",order=order, jitter=False,legend=False)
-vs = {k:[] for k in orderage}
-for i,points in enumerate(ax.collections):
-	vertices = points.get_offsets().data
-	if len(vertices)>0:
-		for ii,a in enumerate([4,7,6,5]):
-			if (i+ii) % 4 == 0:
-				vs[a].append(vertices[0][0])
-for subject in delaydf.Subject.unique():
-	tmp = delaydf[delaydf.Subject == subject]
-	age = tmp['Age'].iloc[0]
-	for i,selection in enumerate(order):
-		data = tmp[tmp.Selection==selection]['Proportion Selected'].iloc[0]
-		ax.scatter(vs[age][i],data,marker='*',color='r',alpha=0.75, s=300,zorder=3)
-handles, labels = ax.get_legend_handles_labels()
-l = plt.legend(handles[0:4], labels[0:4], bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-ax.set(xlabel=None)
-plt.axhline(y=0.25, color='k', linestyle='--')
-fig.savefig(figurepath+'PS_Target_Lure_Foil.png', bbox_inches='tight', dpi=100)
-
-
-# plot correlation between age and Target/Lure/Foil proportion 
-outputdf = pd.DataFrame(output)
-sns.set_theme(style="white", palette=['#6e90bf',  '#d9a6a4', '#c26f6d'])
-g = sns.lmplot(
-    data=outputdf,
-    x="Age", y="Proportion Selected", hue="Selection",
-)
-plt.axhline(y=0.25, color='k', linestyle='--')
-g.savefig(figurepath+'PS_corr_Target_Lure_Foil.png', bbox_inches='tight', dpi=100)
-statlist = []
-for selection in PSkey.values():
-	tmpdf = outputdf[outputdf['Selection'] == selection]
-	res = stats.pearsonr(tmpdf['Age'],tmpdf['Proportion Selected'])
-	statlist.append({'Selection':selection, 'Age/Prop r':res[0], 'Age/Prop p':res[1]})
-statdf=pd.DataFrame(statlist)
-statdf
 
 # cannot run rmANOVA due to unequal group sizes (unequal number of subjects in each age group)
 # Therefore running LMM
